e31b967311a5411b91a92deb7701fa5434ba082d
nguyendinhlong04 <123631942+nguyendinhlong04@users.noreply.github.com>
2025-08-05 17:37:13 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ CLIENT_ID       = os.getenv("CLIENT_ID")
 CLIENT_SECRET   = os.getenv("CLIENT_SECRET")
 DEPLOY_URL      = os.getenv("DEPLOY_URL")
 TABLE_NAME      = os.getenv("TABLE_NAME", "DonHang")
-MAX_RECORDS     = int(os.getenv("MAX_RECORDS") or 100)
+MAX_RECORDS     = int(os.getenv("MAX_RECORDS") or 500)
 
 STATUS_MAPPING = {
     "差出人に返送": "Thất bại - Hoàn hàng",
@@ -23,5 +23,8 @@ STATUS_MAPPING = {
     "持ち出し中": "Đang giao",
     "引受": "Đang vận chuyển",
     "到着": "Đang vận chuyển",
-    "中継": "Đang vận chuyển"
+    "中継": "Đang vận chuyển",
+    "配達希望受付": "Chờ giao",
+    "保管": "Lưu kho"
+
 }
